a5530df91ddd79f44e8fe12d29c03972e61d9934
SebastianZH <holgado.sebastian@gmail.com>
2017-06-10 14:21:40 -0300

    Set up basic gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+	return gulp.src('./src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'src'
+		}
+	});
+});
