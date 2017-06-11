79e309b1566933166eac8cb00a429e1bc6853efc
SebastianZH <holgado.sebastian@gmail.com>
2017-06-11 16:26:01 -0300

    Adds bower files gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,31 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
+var mainBowerFiles = require('gulp-main-bower-files');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('main-bower-files', function() {
+    var filterJS = gulpFilter('**/*.js', { restore: true });
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles({
+            overrides: {
+                bootstrap: {
+                    main: [
+                        './dist/js/bootstrap.js',
+                        './dist/css/*.min.*',
+                        './dist/fonts/*.*'
+                    ]
+                }
+            }
+        }))
+        .pipe(filterJS)
+        .pipe(concat('vendor.js'))
+        .pipe(uglify())
+        .pipe(filterJS.restore)
+        .pipe(gulp.dest('./wwwroot/libs'));
+});
+
 
 gulp.task('sass', function() {
 	return gulp.src('./src/scss/**/*.scss')
@@ -23,8 +48,15 @@ gulp.task('browserSync', function() {
 	});
 });
 
+gulp.task('main-bower-files', function() {
+	return gulp.src('./bower.json')
+    .pipe(mainBowerFiles())
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./src/scripts'));
+});
 
-gulp.task('run', ['browserSync', 'sass'], function() {
+gulp.task('run', ['browserSync', 'sass', 'main-bower-files'], function() {
 	gulp.watch('src/scss/**/*.scss', ['sass']);
 	gulp.watch('src/*.html', browserSync.reload);
   gulp.watch('src/js/**/*.js', browserSync.reload);
