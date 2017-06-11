5d1d6f6932995fe9375cd65796e71290e07924df
SebastianZH <holgado.sebastian@gmail.com>
2017-06-11 00:32:36 -0300

    Adds future of work section. Adds tooltip component

--- src/scripts/controllers/main-controller.js
+++ src/scripts/controllers/main-controller.js
@@ -4,5 +4,32 @@ angular
   .module('hrnLandingApp')
 	.controller('mainController', [
 		function( ) {
+
+      this.highlights = [
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        },
+        {
+          name: 'name1',
+          icon: 'fa fa-home'
+        }
+      ]
 		}
 	]);
