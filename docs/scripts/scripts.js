a3b39db7f577bbd3f26d4fb3a286e2042d235a89
SebastianZH <holgado.sebastian@gmail.com>
2017-06-11 18:54:15 -0300

    Built project

---
+++ docs/scripts/scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("hrnLandingApp",[]),angular.module("hrnLandingApp").controller("mainController",[function(){this.highlights=[{name:"name1",icon:"fa fa-home"},{name:"name1",icon:"fa fa-home"},{name:"name1",icon:"fa fa-home"},{name:"name1",icon:"fa fa-home"},{name:"name1",icon:"fa fa-home"},{name:"name1",icon:"fa fa-home"}]}]);
