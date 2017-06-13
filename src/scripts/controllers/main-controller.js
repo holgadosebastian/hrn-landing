b2ff320f789f86bf3acd0cc25841c9eda66ae9b9
SebastianZH <holgado.sebastian@gmail.com>
2017-06-12 21:48:27 -0300

    Several layout and styles fixes

--- src/scripts/controllers/main-controller.js
+++ src/scripts/controllers/main-controller.js
@@ -7,29 +7,81 @@ angular
 
       this.highlights = [
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'Home',
+            icon: 'fa-home',
+            color: '#9c0'
         },
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'Main Stage',
+            icon: 'fa-microphone',
+            color: '#18a7e0'
         },
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'HR Technology',
+            icon: 'fa-cog',
+            color: '#bb6e3d'
         },
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'Talent & Recruitment',
+            icon: 'fa-eye',
+            color: '#57b1b8'
         },
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'Smart Data',
+            icon: 'fa-pie-chart',
+            color: '#8a5ea4'
         },
         {
-          name: 'name1',
-          icon: 'fa fa-home'
+            name: 'Learning',
+            icon: 'fa-lightbulb-o',
+            color: '#00a29a'
+        },
+        {
+            name: 'Adoption',
+            icon: 'fa-puzzle-piece',
+            color: '#81b041'
+        },
+        {
+            name: 'HR SS & Payroll',
+            icon: 'fa-refresh',
+            color: '#809453'
+        },
+        {
+            name: 'Wellbeing & Rewards',
+            icon: 'fa-database',
+            color: '#a92a31'
+        },
+        {
+            name: 'Digital HR',
+            icon: 'fa-power-off',
+            color: '#ee2a7b'
+        },
+        {
+            name: 'Diversity & Inclusion',
+            icon: 'fa-smile-o',
+            color: '#3b8954'
+        },
+        {
+            name: 'Think Tank',
+            icon: 'fa-home',
+            color: '#3f80c2'
+        },
+        {
+            name: 'Think Tank II',
+            icon: 'fa-home',
+            color: '#3f80c2'
+        },
+        {
+            name: 'disruptHR',
+            icon: 'fa-home',
+            color: '#cca82e'
+        },
+        {
+            name: 'Product Demo',
+            icon: 'fa-star',
+            color: '#e41a52'
         }
-      ]
+    ];
+
 		}
 	]);
